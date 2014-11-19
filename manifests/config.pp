52f35bf95ee7878c97ca377364fceaccbd51f213
Gaudenz Steinlin <gaudenz.steinlin@cirrax.com>
2014-11-19 14:43:21 +0100

    Avoid unqualified variables

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,12 +3,12 @@
 # Installs configuration files
 class libvirt::config inherits libvirt {
 
-  if ($qemu_hook) {
+  if ($libvirt::qemu_hook) {
     file {"${params::config_dir}/hooks/qemu":
       owner  => 'root',
       group  => 'root',
       mode   => '0755',
-      source => "puppet:///modules/libvirt/hooks/qemu/${qemu_hook}",
+      source => "puppet:///modules/libvirt/hooks/qemu/${libvirt::qemu_hook}",
     }
   }
 }
