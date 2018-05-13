475f2eb25d11244aecc6538174762302cf2e035e
Raoul Bhatia <raoul@bhatia.at>
2018-05-13 21:11:23 +0200

    Add support for Ubuntu 18.04/Bionic Beaver
    libvirt-bin is a transitional package.

--- manifests/params.pp
+++ manifests/params.pp
@@ -25,6 +25,10 @@ class libvirt::params {
           $libvirt_package_names  = ['libvirt-bin']
           $service_name           = 'libvirtd'
         }
+        'bionic': {
+          $libvirt_package_names  = ['libvirt-daemon-system', 'libvirt-clients']
+          $service_name           = 'libvirtd'
+        }
         default: {
           fail("${::lsbdistcodename} is currently not supported by the libvirt
                 module. Please add support for it and submit a patch!")
