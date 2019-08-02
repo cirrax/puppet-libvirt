f2248dcd7fe050b7fe6844760aafc47d2cc78205
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2019-08-02 17:56:19 +0200

    add spec test for libvirt_generate_uuid function

---
+++ spec/functions/libvirt_generate_uuid_spec.rb
@@ -0,0 +1,11 @@
+
+require 'spec_helper'
+
+describe 'libvirt_generate_uuid' do
+
+  context "with parameters" do
+    it { should run.with_params('generate a uuid').and_return('ccb5557f-4aea-b88d-1a57-be7b3909e216') }
+    it { should run.with_params('generate another uuid').and_return('57ca0a6e-d282-38a4-24fe-e7c8eb1c89b8') }
+  end
+end
+
