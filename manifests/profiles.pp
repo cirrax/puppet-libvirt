5ff6341dab0758f883d03e50e67c8b87dca3116b
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2022-05-02 17:23:36 +0200

    add comments in libvirt::profiles class

--- manifests/profiles.pp
+++ manifests/profiles.pp
@@ -4,8 +4,12 @@
 #
 # @param devices
 #   devices profiles to load
+#   remark: parameter is hiera hash merged
 # @param domconf
 #   domconf profiles to load
+#   remark: parameter is hiera hash merged
+#
+# find some default profiles in the data/profiles directory 
 #
 class libvirt::profiles (
   Hash $devices,
