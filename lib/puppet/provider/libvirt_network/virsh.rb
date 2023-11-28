64ffbbcce2ae78eef13dadbaf377e2b86b924fc0
Anthony Hinsinger <anthony.hinsinger@gmail.com>
2023-11-28 21:45:25 +0100

    Fixed pdk code conventions

--- lib/puppet/provider/libvirt_network/virsh.rb
+++ lib/puppet/provider/libvirt_network/virsh.rb
@@ -93,8 +93,8 @@ Puppet::Type.type(:libvirt_network).provide(:virsh) do
     xml.root.elements.delete('//mac')
     # remove the connections
     xml.root.attributes.delete('connections')
-    xml.root.elements.each("//forward/interface[@connections]") do |el|
-      el.attributes.delete('connections');
+    xml.root.elements.each('//forward/interface[@connections]') do |el|
+      el.attributes.delete('connections')
     end
     formatter = REXML::Formatters::Pretty.new(2)
     formatter.compact = true
