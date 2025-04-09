70c6846e259f11d00f2c4db9ad8ed9c519a23717
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2025-04-09 11:51:40 +0200

    Revert partly "human readable diff in case of changes; correct sort for portgroups inside XML"
    This partly reverts commit 4c559086bde670435a1fd83ba9acfecf2d93f54a (#75).
    
    The part 'human readable diff' is kept, the part 'correct sort...' is reverted.
    
    Fixing the sort in the provider only is the wrong approach. This module uses
    the function defined in lib/puppet_x/libvirt/sort_elements.rb to
    have equal XML's to compare. The sort_elements function is used in the provider
    to ensure sorting of the 'is' part. The 'should' part is sorted using
    the function libirt::normalxml on the template XML output.
    
    See Bug #85

--- lib/puppet/provider/libvirt_network/virsh.rb
+++ lib/puppet/provider/libvirt_network/virsh.rb
@@ -99,16 +99,7 @@ Puppet::Type.type(:libvirt_network).provide(:virsh) do
     formatter = REXML::Formatters::Pretty.new(2)
     formatter.compact = true
     output = ''.dup
-
-    xml = recursive_sort(xml.root)
-    sorted_portgroups = xml.root.get_elements('//portgroup').sort_by { |obj| obj.attributes['name'] }
-
-    REXML::XPath.match(xml, '//portgroup').each(&:remove)
-    sorted_portgroups.each do |pg|
-      xml.root.add_element pg
-    end
-
-    formatter.write(xml.root, output)
+    formatter.write(recursive_sort(xml.root), output)
     output
   end
 
