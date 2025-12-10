ff24e916798fa2f264480c9dd57d5b1e70c183d5
Benedikt Trefzer <benedikt.trefzer@cirrax.com>
2025-12-10 16:27:15 +0100

    refactor the libvirt_generate_mac_addresses function
    Include the 'source' key if available generate unique
    mac adresses for interface configs which are not
    including network name and portgroup by using the
    whole interface configuration (without the filter
    setting).
    
    Attention: these changes have the potetial to change
    your mac adresses upon upgrade !

--- spec/functions/libvirt_generate_mac_addresses_spec.rb
+++ spec/functions/libvirt_generate_mac_addresses_spec.rb
@@ -37,10 +37,54 @@ describe 'libvirt_generate_mac_addresses' do
                                            { 'network' => 'test', 'portgroup' => 'he' },
                                            { 'network' => 'test', 'portgroup' => 'll', 'mac' => '42:42:42:42:42:42' },
                                            { 'network' => 'prod', 'portgroup' => 'oo' },
+                                           { 'network' => 'nopo' },
                                          ]).and_return([
                                                          { 'network' => 'test', 'portgroup' => 'he', 'mac' => '52:54:00:d8:54:bc' },
                                                          { 'network' => 'test', 'portgroup' => 'll', 'mac' => '42:42:42:42:42:42' },
                                                          { 'network' => 'prod', 'portgroup' => 'oo', 'mac' => '52:54:00:b8:cf:03' },
+                                                         { 'network' => 'nopo', 'mac' => '52:54:00:14:ec:f8' },
+                                                       ])
+        }
+      end
+
+      context 'with interface source and predefined mac' do
+        it {
+          is_expected.to run.with_params([
+                                           { 'source' => { 'network' => 'test' }, 'mac' => '42:42::42:42:42:42' },
+                                         ]).and_return([
+                                                         { 'source' => { 'network' => 'test' }, 'mac' => '42:42::42:42:42:42' },
+                                                       ])
+        }
+      end
+
+      context 'with multiple mixed source interfaces' do
+        it {
+          is_expected.to run.with_params([
+                                           { 'source' => { 'network' => 'test', 'portgroup' => 'he' } },
+                                           { 'source' => { 'network' => 'test', 'portgroup' => 'll' }, 'mac' => '42:42:42:42:42:42' },
+                                           { 'source' => { 'network' => 'prod', 'portgroup' => 'oo' } },
+                                           { 'source' => { 'network' => 'nopo' } },
+                                         ]).and_return([
+                                                         { 'source' => { 'network' => 'test', 'portgroup' => 'he' }, 'mac' => '52:54:00:d8:54:bc' },
+                                                         { 'source' => { 'network' => 'test', 'portgroup' => 'll' }, 'mac' => '42:42:42:42:42:42' },
+                                                         { 'source' => { 'network' => 'prod', 'portgroup' => 'oo' }, 'mac' => '52:54:00:b8:cf:03' },
+                                                         { 'source' => { 'network' => 'nopo' }, 'mac' => '52:54:00:14:ec:f8' },
+                                                       ])
+        }
+      end
+
+      context 'with ignoring filter rules' do
+        it {
+          is_expected.to run.with_params([
+                                           { 'interface_type' => 'bridge', 'source' => { 'bridge' => 'virbr0' } },
+                                           { 'interface_type' => 'bridge', 'source' => { 'bridge' => 'virbr0' }, 'filter' => 'clean-traffic' },
+                                           { 'interface_type' => 'user' },
+                                           { 'interface_type' => 'user', 'filter' => 'clean-traffic' },
+                                         ]).and_return([
+                                                         { 'interface_type' => 'bridge', 'source' => { 'bridge' => 'virbr0' }, 'mac' => '52:54:00:79:e5:40' },
+                                                         { 'interface_type' => 'bridge', 'source' => { 'bridge' => 'virbr0' }, 'filter' => 'clean-traffic', 'mac' => '52:54:00:79:e5:40' },
+                                                         { 'interface_type' => 'user', 'mac' => '52:54:00:12:36:36' },
+                                                         { 'interface_type' => 'user', 'filter' => 'clean-traffic', 'mac' => '52:54:00:12:36:36' },
                                                        ])
         }
       end
